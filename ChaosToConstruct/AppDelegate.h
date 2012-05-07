6012e92a24068fa01a3bf5f676bd9db9baca5a3a
will zhang <will@localhost.(none)>
2012-05-07 22:45:41 +0800

    Initial Commit

---
+++ ChaosToConstruct/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChaosToConstruct
+//
+//  Created by zhang will on 12-5-7.
+//  Copyright (c) 2012年 WiStone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
